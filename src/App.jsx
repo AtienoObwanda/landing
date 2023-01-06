ec7208920fa5dfc9167709ed67ee7d89c3eb6009
atienoobwanda <atienoobwanda@gmail.com>
2023-01-06 08:21:08 +0300

    basic boiler plate layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-       
-      <h1> Landing </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-      
-    </div>
+    <div>App</div>
   )
 }
 
